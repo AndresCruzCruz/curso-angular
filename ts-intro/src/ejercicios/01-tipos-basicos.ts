6eb571b942529733686473b275885b08a04df0f6
Andrés Fernando Cruz Cruz <isc.andres.cruz@gmail.com>
2024-06-26 19:42:22 -0600

    Update curso

--- ts-intro/src/ejercicios/01-tipos-basicos.ts
+++ ts-intro/src/ejercicios/01-tipos-basicos.ts
@@ -2,6 +2,8 @@
     ===== Código de TypeScript =====
 */
 
+// Let es para declarar variables que pueden cambiar su valor
+//Const es para declarar variables que no pueden cambiar su valor
 let nombre: string = 'Strider';
 let hp: number | string = 95;
 let estaVivo: boolean = false;
